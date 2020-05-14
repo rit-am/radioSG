c1ab9ce065e2c6402905d804cd8802c7830496aa
ritam <38151929+rit-am@users.noreply.github.com>
2020-05-15 04:37:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 
-var gumStream;                                          //stream from getUserMedia()
-var rec;                                                        //Recorder.js object
-var input;                                                      //MediaStreamAudioSourceNode we'll be recording
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
 
-// shim for AudioContext when it's not avb.
+// shim for AudioContext when it's not avb. 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext //audio context to help us record
 
@@ -45,183 +45,183 @@ varPhone = document.getElementById("Phone").value;
 
 
 
+	
+	console.log("submitData clicked");
+	recordButton.disabled = false;
+	recordButton.contentEditable = true;
+	submitButton.disabled = true;
 
-        console.log("submitData clicked");
-        recordButton.disabled = false;
-        recordButton.contentEditable = true;
-        submitButton.disabled = true;
-
-        console.log("Ready to accept " +
-                ":: Track Name - " + varTrackName +
-                ":: Track Description - " + varTrackDescription +
-                ":: Category - " + varCategory +
-                ":: Artist - " + varArtistName+
-                ":: Phone - " + varPhone
-                );
+	console.log("Ready to accept " +  
+		":: Track Name - " + varTrackName +
+		":: Track Description - " + varTrackDescription + 
+		":: Category - " + varCategory + 
+		":: Artist - " + varArtistName+
+		":: Phone - " + varPhone
+		);
 
 }
 
 function startRecording() {
-        console.log("recordButton clicked");
+	console.log("recordButton clicked");
 
-        /*
-                Simple constraints object, for more advanced audio features see
-                https://addpipe.com/blog/audio-constraints-getusermedia/
-        */
-
+	/*
+		Simple constraints object, for more advanced audio features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
     var constraints = { audio: true, video:false }
 
-        /*
-        Disable the record button until we get a success or fail from getUserMedia()
-        */
+ 	/*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
 
-        recordButton.disabled = true;
-        console.log("record disabled");
-        stopButton.disabled = false;
-        console.log("stop enabled");
-        pauseButton.disabled = false;
-        console.log("pause enabled");
+	recordButton.disabled = true;
+	console.log("record disabled");
+	stopButton.disabled = false;
+	console.log("stop enabled");
+	pauseButton.disabled = false;
+	console.log("pause enabled");
 
 
-        /*
-        We're using the standard promise based getUserMedia()
-        https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-        */
+	/*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
 
-        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-                console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
-                /*
-                        create an audio context after getUserMedia is called
-                        sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-                        the sampleRate defaults to the one set in your OS for your playback device
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
 
-                */
-                audioContext = new AudioContext();
+		*/
+		audioContext = new AudioContext();
 
-                //update the format
-                document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+		//update the format 
+		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
 
-                /*  assign to gumStream for later use  */
-                gumStream = stream;
+		/*  assign to gumStream for later use  */
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
 
-                /* use the stream */
-                input = audioContext.createMediaStreamSource(stream);
+		/* 
+			Create the Recorder object and configure to record mono sound (1 channel)
+			Recording 2 channels  will double the file size
+		*/
+		rec = new Recorder(input,{numChannels:1})
 
-                /*
-                        Create the Recorder object and configure to record mono sound (1 channel)
-                        Recording 2 channels  will double the file size
-                */
-                rec = new Recorder(input,{numChannels:1})
+		//start the recording process
+		rec.record()
 
-                //start the recording process
-                rec.record()
+		console.log("Recording started");
 
-                console.log("Recording started");
-
-        }).catch(function(err) {
-                //enable the record button if getUserMedia() fails
-        recordButton.disabled = false;
-        stopButton.disabled = true;
-        pauseButton.disabled = true
-        });
+	}).catch(function(err) {
+	  	//enable the record button if getUserMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+    	pauseButton.disabled = true
+	});
 }
 
 function pauseRecording(){
-        console.log("pauseButton clicked rec.recording=",rec.recording );
-        if (rec.recording){
-                //pause
-                rec.stop();
-                pauseButton.innerHTML="Resume";
-        }else{
-                //resume
-                rec.record()
-                pauseButton.innerHTML="Pause";
+	console.log("pauseButton clicked rec.recording=",rec.recording );
+	if (rec.recording){
+		//pause
+		rec.stop();
+		pauseButton.innerHTML="Resume";
+	}else{
+		//resume
+		rec.record()
+		pauseButton.innerHTML="Pause";
 
-        }
+	}
 }
 
 function stopRecording() {
-        console.log("stopButton clicked");
+	console.log("stopButton clicked");
 
-        //disable the stop button, enable the record too allow for new recordings
-        stopButton.disabled = true;
-        recordButton.disabled = true;
-        pauseButton.disabled = true;
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = true;
+	pauseButton.disabled = true;
         submitButton.contentEditable = true;
         submitButton.disabled = false;
 
 
 
-        //reset button just in case the recording is stopped while paused
-        pauseButton.innerHTML="Pause";
+	//reset button just in case the recording is stopped while paused
+	pauseButton.innerHTML="Pause";
+	
+	//tell the recorder to stop the recording
+	rec.stop();
 
-        //tell the recorder to stop the recording
-        rec.stop();
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
 
-        //stop microphone access
-        gumStream.getAudioTracks()[0].stop();
-
-        //create the wav blob and pass it on to createDownloadLink
-        rec.exportWAV(createDownloadLink);
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
 }
 
 function createDownloadLink(blob) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
 
-        var url = URL.createObjectURL(blob);
-        var au = document.createElement('audio');
-        var li = document.createElement('li');
-        var link = document.createElement('a');
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
 
-        //name of .wav file to use during upload and download (without extendion)
-        var filename = new Date().toISOString();
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
 
-        //add controls to the <audio> element
-        au.controls = true;
-        au.src = url;
+	//save to disk link
+	link.href = url;
+	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.innerHTML = "Save to disk";
 
-        //save to disk link
-        link.href = url;
-        link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-        link.innerHTML = "Save to disk";
+	//add the new audio element to li
+	li.appendChild(au);
+	
+	//add the filename to the li
+	li.appendChild(document.createTextNode(filename+".wav "))
 
-        //add the new audio element to li
-        li.appendChild(au);
-
-        //add the filename to the li
-        li.appendChild(document.createTextNode(filename+".wav "))
-
-        //add the save to disk link to li
-        li.appendChild(link);
+	//add the save to disk link to li
+	li.appendChild(link);
 
 
-
-        //upload link
-        var upload = document.createElement('a');
-        upload.href="#";
-        upload.innerHTML = "Upload";
-        upload.addEventListener("click", function(event){
-                  var xhr=new XMLHttpRequest();
-                  xhr.onload=function(e) {
-                      if(this.readyState === 4) {
-                          console.log("Server returned: ",e.target.responseText);
-                      }
-                  };
-                  var fd=new FormData();
-                  fd.append("audio_data",blob, filename);
-                  xhr.open("POST","upload.php",true);
-                  xhr.send(fd);
+	
+	//upload link
+	var upload = document.createElement('a');
+	upload.href="#";
+	upload.innerHTML = "Upload";
+	upload.addEventListener("click", function(event){
+		  var xhr=new XMLHttpRequest();
+		  xhr.onload=function(e) {
+		      if(this.readyState === 4) {
+		          console.log("Server returned: ",e.target.responseText);
+		      }
+		  };
+		  var fd=new FormData();
+		  fd.append("audio_data",blob, filename);
+		  xhr.open("POST","./../../php/pushrecordjswav.php",true);
+		  xhr.send(fd);
 
 
         console.log("Procssing Metadata");
-                 updatedb(varTrackName,varTrackDescription,varCategory,varArtistName,varPhone,filename);
+		 updatedb(varTrackName,varTrackDescription,varCategory,varArtistName,varPhone,filename);
 
-        })
-        li.appendChild(document.createTextNode (" "))//add a space in between
-        li.appendChild(upload)//add the upload link to li
+	})
+	li.appendChild(document.createTextNode (" "))//add a space in between
+	li.appendChild(upload)//add the upload link to li
 
-        //add the li element to the ol
-        recordingsList.appendChild(li);
+	//add the li element to the ol
+	recordingsList.appendChild(li);
 }
 
   function updatedb(varTrackName,varTrackDescription,varCategory,varArtistName,varPhone,filename) {
@@ -232,9 +232,9 @@ function createDownloadLink(blob) {
                 console.log(this.responseText);
             }
         }
-        a.open("POST", "data.php", true);
+        a.open("POST", "./../../php/updatemediatable.php", true);
         a.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         a.send("TrackName=" + varTrackName + "&TrackDescription=" + varTrackDescription + "&Category=" + varCategory + "&Artist=" + varArtistName + "&Phone=" + varPhone + "&File=" + filename);
-        console.log("Sending Metadata");
-        alert("Thank you for the submission. The Track will be forwarded to our moderators for approval. Please feel free to submit more Tracks!");
+	console.log("Sending Metadata");
+	alert("Thank you for the submission. The Track will be forwarded to our moderators for approval. Please feel free to submit more Tracks!");
     }
